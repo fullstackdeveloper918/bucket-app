07a7a10be92ffe1fe72a355a3560a90d8d9da92e
fullstackdeveloper918 <34833947+fullstackdeveloper918@users.noreply.github.com>
2025-04-28 21:26:59 +1000

    Delete prisma/migrations/20250206055003_init_mew_columns_in_volume directory

--- prisma/migrations/20250206055003_init_mew_columns_in_volume/migration.sql
+++
@@ -1,7 +0,0 @@
--- DropForeignKey
-ALTER TABLE `sales` DROP FOREIGN KEY `Sales_bundleId_fkey`;
-
--- AlterTable
-ALTER TABLE `volumediscount` ADD COLUMN `postion` JSON NULL,
-    ADD COLUMN `product_id` INTEGER NULL,
-    ADD COLUMN `section` JSON NULL;
